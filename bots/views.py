56821bd680aa42484601565064b457f4f67c5084
Suhrob <oopanndaa@gmail.com>
2021-10-26 18:41:58 +0500

    changes

--- bots/views.py
+++ bots/views.py
@@ -10,11 +10,8 @@ from .models import Bot
 def process_telegram_updates(request, bot_token):
     if request.method == "POST":
         try:
-            print(bot.message_handlers)
             bot.token = Bot.objects.get(token=bot_token).token
-            update = types.Update.de_json(request.body.decode('utf-8'))
-            print(update)
-            bot.process_new_updates([update])
+            bot.process_new_updates([types.Update.de_json(request.body.decode('utf-8'))])
             return JsonResponse({}, status=200)
         except Bot.DoesNotExist:
             return JsonResponse({'detail': 'Invalid token'}, status=400)
